1dd75e0650ba8393518265c4d414f684eef12235
zainabkarim18 <zainabkarim18@github.com>
2024-09-03 20:43:27 +0300

    Assignment Completed

--- clues.sql
+++ clues.sql
@@ -1,34 +1,60 @@
 -- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
- 
--- Write SQL query here
+
+SELECT code,name, population, region FROM countries WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
+
+
+--  code |             name              | population |     region      
+-- ------+-------------------------------+------------+-----------------
+--  VAT  | Holy See (Vatican City State) |       1000 | Southern Europe
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
--- Write SQL query here
+SELECT code,name,language,isofficial,population FROM countrylanguages JOIN countries ON code = countrycode WHERE region = 'Southern Europe' ORDER BY population ASC LIMIT 1;
 
+--  code |             name              | language | isofficial | population 
+-- ------+-------------------------------+----------+------------+------------
+--  VAT  | Holy See (Vatican City State) | Italian  | t          |       1000
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
--- Write SQL query here
+SELECT code,name, language,isofficial,population FROM countries JOIN countrylanguages ON code = countrycode WHERE region='Southern Europe' AND language='Italian' AND percentage=100;
 
+--  code |    name    | language | isofficial | population 
+-- ------+------------+----------+------------+------------
+--  SMR  | San Marino | Italian  | t          |      27000
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
--- Write SQL query here
+SELECT code, cities.name, countrylanguages.language  FROM cities JOIN countries ON cities.countrycode = countries.code JOIN countrylanguages ON countries.code = countrylanguages.countrycode WHERE cities.name != 'San Marino' AND countries.name = 'San Marino';
 
+-- code |    name    | language 
+-- ------+------------+----------
+--  SMR  | Serravalle | Italian
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
--- Write SQL query here
+SELECT code,cities.name, district, cities.population FROM cities JOIN countries ON code = countrycode WHERE continent ='South America' AND cities.name != 'Serravalle' AND cities.name LIKE 'Serr%';
 
+-- code |  name  |    district    | population 
+-- ------+--------+----------------+------------
+--  BRA  | Brazil | Espírito Santo |     302666
+
+SELECT code,countries.name , district, cities.population FROM countries JOIN cities on code = countrycode WHERE cities.name='Serra';
+
+--  code |  name  |    district    | population 
+-- ------+--------+----------------+------------
+--  BRA  | Brazil | Espírito Santo |     302666
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
--- Write SQL query here
+SELECT code,cities.name, district, cities.population FROM cities JOIN countries ON countries.capital = cities.id WHERE countries.name='Brazil';
 
+-- code |   name   |     district     | population 
+-- ------+----------+------------------+------------
+--  BRA  | Brasília | Distrito Federal |    1969868
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
@@ -42,3 +68,9 @@
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT * FROM cities WHERE population = 91084;
+
+--   id  |     name     | countrycode |  district  | population 
+-- ------+--------------+-------------+------------+------------
+--  4060 | Santa Monica | USA         | California |      91084
